1e7bd168fb0b384c3591eac8ee53fb1bcc4e4d67
damistydesign <damielprogamer416@gmail.com>
2025-09-05 13:21:40 -0300

    Nuevo Reto!

---
+++ Week 2/Day 1/SortByAgeInArray.js
@@ -0,0 +1,18 @@
+const edades = [
+  {
+    nombre: 'leo',
+    edad: 38
+  },
+  {
+    nombre: 'luis',
+    edad: 37
+  },
+  {
+    nombre: 'dami',
+    edad: 17
+  }
+]
+
+edades.sort((a, b) => b.edad - b.edad)
+
+console.log(edades)
